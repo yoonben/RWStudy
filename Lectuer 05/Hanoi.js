b515e2b94abeea72355461a4f08409c81f9c4809
82103 <82103@DESKTOP-E84VCIN>
2022-04-12 22:03:26 +0900

    [ES] 5강 업로드

---
+++ Lectuer 05/Hanoi.js
@@ -0,0 +1,18 @@
+    
+       function clickEvent1()
+        {
+            alert("첫번째 상자를 클릭하셨습니다.");
+        }
+        function clickEvent2()
+        {
+            alert("두번째 상자를 클릭하셨습니다.");
+        }
+        function clickEvent3()
+        {
+            alert("세번째 상자를 클릭하셨습니다.");
+        }
+        function dis(){
+            document.getElementById("hide").style.display = "none";
+        }
+    
+    
